ae6f617d0ed55582ae7255325ec8d88669404cd1
kangshwan <rnltls97@gmail.com>
2023-09-27 22:01:29 +0900

    Version Change

--- src/main/java/com/rnltls/oauth/domain/Member.java
+++ src/main/java/com/rnltls/oauth/domain/Member.java
@@ -1,14 +1,13 @@
 package com.rnltls.oauth.domain;
 
-
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
+//import jakarta.persistence.*;
 import javax.persistence.*;
-
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 만들어줌
 @DynamicUpdate //update 할때 실제 값이 변경됨 컬럼으로만 update 쿼리를 만듬
